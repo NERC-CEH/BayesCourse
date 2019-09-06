a1bcad75105fa8b35b6c4e360baf77394259defa
Peter Levy <plevy@ceh.ac.uk>
2019-09-06 11:03:26 +0100

    Add files via upload

---
+++ BayesianCourse_Software.R
@@ -0,0 +1,25 @@
+# SOFTWARE TO INSTALL FOR THE COURSE ON
+# 'Bayesian Methods for Ecological and Environmental Modelling'
+
+
+## Begin by installing the following software:
+   # R:       https://www.r-project.org
+   # RStudio: https://www.rstudio.com
+   # JAGS:    http://mcmc-jags.sourceforge.net/
+   # R-INLA:  http://www.r-inla.org/download
+
+
+## Then install the following R-packages (you can run these lines in R):
+install.packages('BayesianTools')
+install.packages('coda')
+install.packages('emdbook')
+install.packages('geoR')
+install.packages('ggmap')
+install.packages('ggplot2')
+install.packages('inlabru')
+install.packages('jagsUI')
+install.packages('mvtnorm')
+install.packages('rjags')
+install.packages('rstanarm')
+install.packages('R2jags')
+install.packages('shiny')
